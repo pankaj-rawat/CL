3c4cd325eb3ffed7645d135bbf8575941bac37cd
pankaj-rawat <messagetopankaj@gmail.com>
2016-08-17 23:26:34 -0700

    schema change and lookup data

--- Database/lookup insert.sql
+++ Database/lookup insert.sql
@@ -1,4 +1,5 @@
 INSERT INTO Country (ID,name,abbr) VALUES(1,'India','IN');
+INSERT INTO Country (ID,name,abbr) VALUES(2,'United States','US');
 
 INSERT INTO STATE(ID,name,idCountry,abbr) VALUES(1,'Uttarakhand',1,'UK');
 
@@ -12,5 +13,11 @@ INSERT INTO Status(id,value) VALUES(4,'Verification Pending');
 INSERT INTO Status(id,value) VALUES(5,'Reported data incorrect');
 INSERT INTO Status(id,value) VALUES(6,'Blocked');
 
+INSERT INTO acceptanceType(id,value) VALUES(1,'Like');
+INSERT INTO acceptanceType(id,value) VALUES(2,'Dislike');
+INSERT INTO acceptanceType(id,value) VALUES(3,'Average');
 
-
+INSERT INTO role(id,value) VALUES(1,'guest');
+INSERT INTO role(id,value) VALUES(2,'admin');
+INSERT INTO role(id,value) VALUES(3,'bussinessowner');
+INSERT INTO role(id,value) VALUES(4,'registereduser');
